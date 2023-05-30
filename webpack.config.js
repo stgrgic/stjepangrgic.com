66b1cd21a37ea1b3e36ef47a8954dc0e76a6884a
Stjepan Grgic <grgic.stjepan@gmail.com>
2023-05-30 18:51:43 +0100

    Webpack updated, copying works

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,19 @@ const IS_DEVELOPMENT = process.env.NODE_ENV === "dev";
 const dirApp = path.join(__dirname, "app");
 const dirAssets = path.join(__dirname, "asstest");
 const dirStyles = path.join(__dirname, "styles");
+const dirNode = "node_modules";
 
-console.log(dirApp, dirAssets, dirStyles);
+module.exports = {
+  entry: [path.join(dirApp, "index.js"), path.join(dirStyles, "index.scss")],
+  resolve: {
+    modules: [dirApp, dirAssets, dirStyles],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      IS_DEVELOPMENT,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "./assets", to: "" }],
+    }),
+  ],
+};
